8f18a60ac745201c3d34077017b288d6f8af345c
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-05-26 19:45:23 +0300

    refactored controller and service

--- src/services/superhero.service.ts
+++ src/services/superhero.service.ts
@@ -1,46 +1,46 @@
 import { Superhero } from '@prisma/client';
 import { prisma } from '../database';
 
-export const getHeroById = (superheroId: string) => {
+export const findSuperhero = (superheroId: string) => {
   return prisma.superhero.findUnique({
     where: { id: superheroId },
   });
 };
 
-export const getHeroes = (page: number, limit: number) => {
+export const findSuperheroes = (page: number, limit: number) => {
   return prisma.superhero.findMany({
     skip: (page - 1) * limit,
     take: limit,
     orderBy: {
       updated_at: 'desc',
-    }
+    },
   });
 };
 
-export const createHero = (
-  data: Omit<Superhero, 'id' | 'updated_at' | 'created_at'>
+export const createSuperhero = (
+  input: Omit<Superhero, 'id' | 'updated_at' | 'created_at'>,
 ) => {
   return prisma.superhero.create({
-    data,
+    data: input,
   });
 };
 
-export const updateHeroById = (
+export const updateSuperhero = (
   superheroId: string,
-  data: Omit<Superhero, 'id' | 'updated_at' | 'created_at'>,
+  input: Omit<Superhero, 'id' | 'updated_at' | 'created_at'>,
 ) => {
   return prisma.superhero.update({
     where: { id: superheroId },
-    data,
+    data: input
   });
 };
 
-export const deleteHeroById = (superheroId: string) => {
+export const deleteSuperhero = (superheroId: string) => {
   return prisma.superhero.delete({
     where: { id: superheroId },
   });
 };
 
-export const getCount = () => {
+export const getSuperheroCount = () => {
   return prisma.superhero.count();
 };
